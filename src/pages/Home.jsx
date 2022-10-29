85a919018e510c6d2f998d6bdf2285b12d74cf54
Jojo <jojothomas1515@gmail.com>
2022-10-29 19:50:46 +0100

    styling ongoing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,18 @@ function Home(props) {
     return (
         <div className={'container'}>
             <div className="profile__info">
-                <div className="profile_img_con"><img id={'profile__img'} src={require('../assets/48474390.jpeg')}
-                                                      alt="profile_img"/>
-                </div>
+                <img id={'profile__img'} src={require('../assets/48474390.jpeg')}
+                     alt="profile_img"/>
+
                 <h4 id={'twitter'}>Joseph Thomas</h4>
                 <h4 id="slack">Joseph Thomas Ehigie</h4>
+                <img id={'share_btn'} src={require('../assets/Icon.png')} alt=""/>
+                <img id={'more_menu'} src={require('../assets/Type=more, State=Default.png')} alt="more menu"/>
 
             </div>
 
             <div className="link_section">
-                <a href="https://twitter.com/jojothomas1515">Jojo Thomas Twitter</a>
+                <a href="https://twitter.com/jojothomas1515">Twitter: @jojothomas1515</a>
                 <a id={'btn_zuri'} href={'https://training.zuri.team/'}>Zuri Team</a>
                 <a id={'books'} href={'http://books.zuri.team/'}>Zuri Books</a>
                 <a id={'book_python'} href={'https://books.zuri.team/python-for-beginners?ref_id=yourslackna'}>Python
@@ -28,12 +30,11 @@ function Home(props) {
                 <a href="https://github.com/jojothomas1515" className="git"><img src={require('../assets/Icongit.png')}
                                                                                  alt="git"/></a>
             </div>
-
             <footer>
-                <div className={'foot-con'}>
+                <div className={'foot_con'}>
                     <img src={require('../assets/Zuri.Internship_Logo.png')} alt="" className="logo"/>
                     <p className={'text-secondary'}>HNG Internship 9 Frontend Task</p>
-                    <img src={require('../assets/Zuri.Internship_Logo.png')} alt="" className="logo"/>
+                    <img src={require('../assets/I4G.png')} alt="" className="logo"/>
                 </div>
             </footer>
         </div>
