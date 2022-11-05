59c0ccd2fa66ce279069c1eb699c08372d1863bc
Jojo <jojothomas1515@gmail.com>
2022-11-05 13:32:00 +0100

    page done , configuring frontend validation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Footer from "../components/Footer";
 import '../styles/Contact.css' // stylesheet importation
 
 function Contact() {
+    const [bd, setBd] = useState(null)
+    const te = useRef()
+    useEffect(()=>{
+        console.log(te.current)
+    })
+    function check(e)
+    {
+        e.preventDefault()
+        if (te.current.value === "")
+        {
+            setBd('err')
+        }
+        else
+        {
+            setBd('')
+        }
+    }
 
     return (<>
 
         <div className={'container'}>
-            <div id={'form__container'}>
+            <form id={'form__container'} onSubmit={e => check(e)}>
                 <div className={'cm'}>
                     <h1>Contact Me</h1>
                     <p>Hi there, contact me to ask me about anything you have in mind.</p>
@@ -27,7 +44,7 @@ function Contact() {
                 </label>
                 <label className={'msg'}>
                     Message
-                    <textarea id={'message'} rows={5}
+                    <textarea id={'message'} rows={5} ref={te} className={bd}
                               placeholder={'Send me a message and I\'ll reply you as soon as possible...'}>
 
                     </textarea>
@@ -38,7 +55,7 @@ function Contact() {
                 </label>
                 <button id={'btn__submit'} className={'btn'}>Send Message</button>
 
-            </div>
+            </form>
         </div>
         <Footer/>
     </>);
